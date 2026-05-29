73a4e463d676bf9dae365a7f5352712cb54a5a80
ulugbek-ghub <ulugbek.github@gmail.com>
2026-05-30 00:22:49 +0500

    js retry

---
+++ 1-Dars/app.js
@@ -0,0 +1,43 @@
+// console.log('Hello World') // string
+// console.log(5)  // number
+// console.log("5") // string
+// console.log(true) // boolean
+// console.log(false) // boolean
+// console.log(null) // null
+// console.log(undefined) // undefined
+
+// console.log(typeof( 5 + '5')); //55
+// console.log(typeof(5 - "5")) // 0
+// console.log(5 * "5") // 25
+
+// console.log(5 + 5) // 10
+
+// Ozgaruvchilar 2 xil boladi: var, let
+
+// let name1 = "John"
+// console.log(name1)
+
+// name1 = "Doe"
+// console.log(name1)
+
+// const name2 = "Jane"
+// console.log(name2)
+
+// name2 = "Smith"
+// console.log(name2)
+
+// ozgarmas constni ozgartirish
+
+// const person = {
+//     name: "Alice",
+//     age: 30
+// }
+
+// console.log(person.name)
+// console.log(person.age)
+
+// person.name = "Bob"
+// person.age = 25
+
+// console.log(person.name)
+// console.log(person.age)
