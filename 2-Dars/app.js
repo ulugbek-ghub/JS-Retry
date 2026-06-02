9bf76e52abb8ba4cc07f70b25c4188a464c70cf3
ulugbek-ghub <ulugbek.github@gmail.com>
2026-06-02 15:51:49 +0500

    2-Dars

---
+++ 2-Dars/app.js
@@ -0,0 +1,80 @@
+//  Malumot turlari
+
+//  var  and  let
+
+// var da elon qilinmasidan oldin chiqarib boladi
+
+// let da elon qilinmasidan oldin chiqarib bolmaydi
+
+// console.log(name3);
+// var name3 = "Charli"
+
+// console.log(name3);
+// let name3 = "Charli"
+
+// {
+//     let city = "New YOrk"
+// }
+// console.log(city);
+
+
+// undefined - Ozgaruvchi elon qilingan, lekin qiymat berilmagan
+
+// Obyekt icida mavjud bolmagan property
+
+// const user = {
+//     age: 25,
+// }
+
+// console.log(user.name);  // undefined
+
+
+// let a = 5
+// let b = "5"
+
+// console.log(a == b); // tenglik
+// console.log(a === b); // qatiy tenglik
+
+// Malumot turlari
+// Oddiy malumot turlari - Primitive data types
+
+// Number
+// String
+// Boolean  true/false
+// Null
+// Undefined
+
+    // Number
+// const num = 78
+// console.log(typeof num);
+// console.log(num);
+
+    // String
+// const str = 'Ra\'no'
+// const str2 = "33"
+
+// console.log(str);
+// console.log(str1);
+
+    // Boolean
+// const isMarried = false
+// console.log(isMarried);
+
+// console.log(null == undefined); //true
+// console.log(null === undefined); //false
+
+// const num
+// console.log(num);
+
+// Object - Kompleks malumot turi
+// Object
+// Array - Massiv
+// Function
+// Error
+
+// Object
+// 1.Kalit(property): 2.Qiymat(Value) - String, Number,Boolean,Undefined
+
+const person = {
+    name = "Asad"
+}
