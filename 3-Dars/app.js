ae5425b5e8077d86e0cdeb4ec180a1729f83131e
ulugbek-ghub <ulugbek.github@gmail.com>
2026-06-04 15:14:09 +0500

    3-Dars

---
+++ 3-Dars/app.js
@@ -0,0 +1,31 @@
+// Shartli operatorlar
+
+const age = +prompt("Yoshingizni kiriting", '');
+
+if(age >= 18 <= 65) {
+    console.log("Siz kattasiz.");
+} 
+else if (age >= 65) {
+    console.log("Siz pensiyanersiz");
+}
+else if (age < 18) {
+    console.log("Siz voyaga yetmagansiz.");
+}
+
+
+// const age = +prompt("Yoshin nechida?");
+
+// if (age >= 18) {
+//     console.log("siz kattasiz.");
+// } else if (age > 12) {
+//     console.log("Siz osmirsiz.");
+// } else {
+//     console.log("Siz hali bolasiiz.");
+// }
+
+
+// const colors = ['red', 'green', 'blue', 'yellow'];
+
+// for (let i = 0; i < colors.length; i++) {
+//     console.log("Rang: " + colors[i]);
+// }
