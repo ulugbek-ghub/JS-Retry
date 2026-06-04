b1d808a50851fb440f477120a04a7323c8f80b36
ulugbek-ghub <ulugbek.github@gmail.com>
2026-06-04 15:39:58 +0500

    3-Dars

--- 3-Dars/app.js
+++ 3-Dars/app.js
@@ -1,16 +1,83 @@
 // Shartli operatorlar
 
-const age = +prompt("Yoshingizni kiriting", '');
+// const age = +prompt("Yoshingizni kiriting", '');
 
-if(age >= 18 <= 65) {
-    console.log("Siz kattasiz.");
-} 
-else if (age >= 65) {
-    console.log("Siz pensiyanersiz");
+// if(age >= 18 <= 65) {
+//     console.log("Siz voyaga yetgansiz.");
+// } 
+// else if (age >= 65) {
+//     console.log("Siz pensiyanersiz");
+// }
+// else if (age < 18) {
+//     console.log("Siz voyaga yetmagansiz.");
+// }
+
+
+// if (age > 18) {
+//     console.log("Siz yoshsiz.");
+// }
+// else if (age >= 18 && age <= 65) {
+//     console.log("Siz katta yoshlisiz");
+// }
+// else {
+//     log("Siz pensiyanersiz");
+// }
+
+// const a = true
+// const b = false
+
+// console.log(a || b)
+
+// 
+
+
+// switch cases
+
+// let svitafor = 'green'
+// switch (svitafor) {
+//     case 'red':
+//         console.log('stop');
+//         break
+    
+//     case 'yellow':
+//         console.log('tayyorlan');
+//         break
+            
+//     case 'green':
+//         console.log('gazini boss');
+//         break
+// }
+
+let day = 5
+switch (day) {
+    case 1:
+        console.log(day, "Dushanba");
+        break
+
+    case 2:
+        console.log(day, "seshanba");
+        break
+    
+    case 3:
+        console.log(day, 'chorshanba')
+        break
+        
+    case 4:
+        console.log(day, 'payshanba');
+        break
+
+    case 5:
+        console.log(day, 'juma')
+        break
+    
+    case 6:
+        console.log(day, 'shanba')
+        break
+
+    case 7:
+        console.log(day, 'yakshanba')
 }
-else if (age < 18) {
-    console.log("Siz voyaga yetmagansiz.");
-}
+
 
 
 // const age = +prompt("Yoshin nechida?");
